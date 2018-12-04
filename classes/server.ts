b1e7b0f1f6a72e07553aefd069f3171a29c47eea
Fernando Garcia <yofer21@gmail.com>
2018-12-03 21:47:24 -0300

    Fin seccion 5

--- classes/server.ts
+++ classes/server.ts
@@ -32,7 +32,11 @@ export default class Server {
     console.log('Escuchando conexiones - sockets');
 
     this.io.on('connection', cliente => {
-      console.log('Cliente conectado');
+      // Conectar cliente
+      socket.conectarCliente(cliente);
+
+      // Configurar usuario
+      socket.configurarUsuario(cliente, this.io);
 
       // Mensajes
       socket.mensaje(cliente, this.io);
--- routes/router.ts
+++ routes/router.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import Server from '../classes/server';
 
 const router = Router();
 
@@ -13,6 +14,14 @@ router.post('/mensajes', (req: Request, res: Response) => {
   const cuerpo = req.body.cuerpo;
   const de = req.body.de;
 
+  const payload = {
+    de,
+    cuerpo
+  };
+
+  const server = Server.instance;
+  server.io.emit('mensaje-nuevo', payload);
+
   res.json({
     ok: true,
     cuerpo,
@@ -23,8 +32,15 @@ router.post('/mensajes', (req: Request, res: Response) => {
 router.post('/mensajes/:id', (req: Request, res: Response) => {
   const cuerpo = req.body.cuerpo;
   const de = req.body.de;
-
   const id = req.params.id;
+
+  const payload = {
+    de,
+    cuerpo
+  };
+
+  const server = Server.instance;
+  server.io.in(id).emit('mensaje-privado', payload);
 
   res.json({
     ok: true,
